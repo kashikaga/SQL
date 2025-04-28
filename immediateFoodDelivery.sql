d7e07340e8329d94c269cbd513af4792739f3d36
Kashika Gupta <93068969+kashikaga@users.noreply.github.com>
2025-04-28 10:01:21 +0530

    Update immediateFoodDelivery.sql

--- immediateFoodDelivery.sql
+++ immediateFoodDelivery.sql
@@ -1,8 +1,9 @@
 //https://leetcode.com/problems/immediate-food-delivery-ii/description
+  
 Select round(avg(order_date = customer_pref_delivery_date)*100, 2) as immediate_percentage
 from Delivery
 where (customer_id, order_date) in (
-  Select customer_id, min(order_date) 
-  from Delivery
-  group by customer_id
+Select customer_id, min(order_date) 
+from Delivery
+group by customer_id
 );
