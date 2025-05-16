9b5d9e70f0701ce97695d994244b963b0794efbc
Kashika Gupta <93068969+kashikaga@users.noreply.github.com>
2025-05-16 20:56:41 +0530

    Create LastPersonFitInBus.sql

---
+++ LastPersonFitInBus.sql
@@ -0,0 +1,8 @@
+//https://leetcode.com/problems/last-person-to-fit-in-the-bus/description
+SELECT person_name
+FROM (SELECT person_name, turn,
+    SUM(weight) OVER (ORDER BY turn) AS cum_wei
+    FROM Queue
+) temp 
+WHERE cum_wei <= 1000
+ORDER BY turn DESC LIMIT 1;
