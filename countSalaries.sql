4d9955465748ab565281430aba2871937e48d623
Kashika Gupta <93068969+kashikaga@users.noreply.github.com>
2025-05-17 17:38:23 +0530

    Create countSalaries.sql

---
+++ countSalaries.sql
@@ -0,0 +1,16 @@
+//https://leetcode.com/problems/count-salary-categories/description/
+
+(
+    select "Low Salary" as category ,
+    (select count(*) from Accounts where income<20000) as accounts_count
+)
+union all
+(
+    select "Average Salary" as category ,
+    (select count(*) from Accounts where income>=20000 and income<=50000) as accounts_count
+)
+union all
+(
+    select "High Salary" as category ,
+    (select count(*) from Accounts where income>50000) as accounts_count
+)
